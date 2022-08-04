0720029d43f2a2a477b303e8e8ae08f68b3260c5
Gowriprasad1 <108853286+Gowriprasad1@users.noreply.github.com>
2022-08-04 12:15:28 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,33 +1,81 @@
-function validpayment(){
-	var cardnumber1=document.paymentform.cardnumber.value;
-	var month=document.paymentform.monthdate.value;
-	var cvv1=document.paymentform.cvv.value;
-	var cardname1=document.paymentform.cardname.value;
-	var phone1=document.paymentform.phone.value;
-
-	if(cardnumber1.length<11){
-		alert("CardNumber Must and Should Contain 12 digits");
-		return false;
-	}
-	else if(month.length>5){
-       alert("Invalid Month and Date");
-       return false;
+function validation() {
+    var card=document.getElementById('cardnumber').value;
+    var month=document.getElementById('month').value;
+    var year=document.getElementById('year').value;
+    var cvv=document.getElementById('cvv').value;
+    var cardname=document.getElementById('cardname').value;
+    var phone=document.getElementById('phone').value;
+  
+   if(card==""){
+    alert("Please Enter CardNumber");
+    return false;
    }
-   else if(cvv1.length>4){
-   	   alert("Invalid Cvv");
-       return false;
+   if(isNaN(card)){
+    alert("CardNumber Only Contain Digits");
+    return false;
    }
-   else if(cvv1.length<2){
-   	   alert("Invalid Cvv");
-       return false;
+   if(card.length!=12){
+     alert("CardNumber Only Contain 12 Digits");
+     return false;
    }
-   else if(cardname1.length<2){
-   	   alert("Card Name Should Contain above 2 letters");
+   if(month==""){
+    alert("Please Enter Month");
+    return false;
    }
-   else if(phone1.length>11){
-   	alert("Phone Number Must and Should Contain only 10 Numbers");
+   if(isNaN(month)){
+    alert("CardNumber Only Contain Digits");
+    return false;
    }
-   else if(phone1.length<9){
-   	alert("Phone Number Must and Should Contain only 10 Numbers");
+   if(month>=13){
+    alert("Invalid Month");
+    return false;
+   }
+   if(year==""){
+    alert("Please Enter Year");
+    return false;
+   }
+   if(isNaN(year)){
+    alert("CardNumber Only Contain Digits");
+    return false;
+   }
+   if(year.length>=5){
+    alert("Invalid Year");
+    return false;
+   }
+   if(year<2022 || year>=2050){
+    alert("Invalid Year");
+    return false;
+   }
+   if(cvv==""){
+    alert("Please Enter Cvv");
+    return false;
+   }
+   if(isNaN(cvv)){
+    alert("CardNumber Only Contain Digits");
+    return false;
+   }
+   if(cvv.length!=3){
+    alert("Invalid CVV");
+    return false;
+   }
+   if(cardname==""){
+    alert("Please Enter CardName");
+    return false;
+   }
+   if(cardname.length<3){
+    alert("CardName Can Contain atlest 3 Charcters");
+    return false;
+   }
+   if(phone==""){
+    alert("Please Enter phone");
+    return false;
+   }
+   if(isNaN(phone)){
+    alert("CardNumber Only Contain Digits");
+    return false;
+   }
+   if(phone.length!=10){
+  alert("PhoneNumber Must Contain 10 Digits");
+  return false;
    }
 }
